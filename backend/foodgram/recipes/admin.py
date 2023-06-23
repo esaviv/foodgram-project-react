aabdf6becfe3df475a671de0d9eddb8777f0412e
Ekaterina Savchenko <eka.saviv@gmail.com>
2023-06-23 17:49:28 +0200

    Импорт данных

--- backend/foodgram/recipes/admin.py
+++ backend/foodgram/recipes/admin.py
@@ -1,3 +1,44 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (Favorite, Ingredient, IngredientRecipe, Recipe,
+                     ShoppingCard, Subscribe, Tag, TagRecipe)
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(IngredientRecipe)
+class IngredientRecipeAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(TagRecipe)
+class TagRecipeAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Favorite)
+class FavoriteAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Subscribe)
+class SubscribeAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(ShoppingCard)
+class ShoppingCardAdmin(admin.ModelAdmin):
+    pass
---
+++ backend/foodgram/recipes/management/commands/import_csv.py
@@ -0,0 +1,30 @@
+import csv
+import os
+
+from django.conf import settings
+from django.core.management.base import BaseCommand
+from recipes.models import Ingredient
+
+DATABASES_DICT = {
+    Ingredient: 'ingredients.csv',
+}
+
+DATA_DIR = os.path.join(settings.BASE_DIR, 'data')
+
+
+class Command(BaseCommand):
+    help = 'Loads the data from csv files located in static/data folder'
+
+    def handle(self, *args, **options):
+        for model, csv_file in DATABASES_DICT.items():
+            with open(
+                os.path.join(DATA_DIR, csv_file), 'r',
+                encoding='utf-8',
+            ) as file:
+                reader = csv.reader(file)
+                next(reader)
+                for data in reader:
+                    model.objects.get_or_create(
+                        name=data[0], measurement_unit=data[1]
+                    )
+        self.stdout.write(self.style.SUCCESS('Successfully loaded data'))
